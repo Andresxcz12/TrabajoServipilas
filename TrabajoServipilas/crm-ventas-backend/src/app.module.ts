1033129f46046efe8ee0c6dde12321177f67d0e0
Andresxcz12 <amayahiguitaandresfelipe@gmail.com>
2026-06-23 20:00:28 -0500

    Actualizar backend: corregir conexión a base de datos y limpiar Firebase

--- TrabajoServipilas/crm-ventas-backend/src/app.module.ts
+++ TrabajoServipilas/crm-ventas-backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PedidosController } from './pedidos/pedidos.controller'; // Verifica esta ruta
 import { PedidosService } from './pedidos/pedidos.service';
+import { PedidosModule } from './pedidos/pedidos.module';
 
 @Module({
-  imports: [],
+  imports: [PedidosModule], // Asegúrate de que PedidosModule esté importado correctamente
   controllers: [PedidosController], // <--- ES CRUCIAL QUE ESTÉ AQUÍ
   providers: [PedidosService],
 })
